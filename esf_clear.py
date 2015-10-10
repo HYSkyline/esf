f991b664c31fc049986761767e2d8d41ad5b90ae
双犬子 <HYSkyline@163.com>
2015-10-10 08:37:58 +0800

    esf_clear
    清除当前写入信息

---
+++ esf_clear.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+__author__ = '双犬子'
+
+print "Link Start!"
+
+f = open(r"F:\application\python\esftst.txt", 'w')
+f.writelines("")
+f.close()
+print "text cleared."
+
+print "Link Logout."
